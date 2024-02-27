8954a84f4c53396607d7fb37cac5e979cee89ede
Kavin Prakash Senthil Prabhu <kavin.senthil@itp.objectfrontier.com>
2024-02-27 09:53:09 +0530

    first commit

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+using JenkinsDemo;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            TestMe testMe = new TestMe();
+            string obj1 = testMe.sayJenkins(1);
+            Assert.AreEqual("jenkins", obj1);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            TestMe testMe = new TestMe();
+            string obj2 = testMe.sayJenkins(2);
+            Assert.AreEqual("jenkinsjenkins", obj2);
+        }
+    }
+}
+has context menu
+
+
+has context menu
